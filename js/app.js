32413ae1e8ac4b2075d09b1466ecfd682e6c7f66
Gabriel Fletcher <gabriel.fletcher@csueastbay.edu>
2019-11-12 14:33:15 -0800

    change

--- js/app.js
+++ js/app.js
@@ -1,21 +1,19 @@
 var canvas = document.getElementById("canvas");
-var counter = document.getElementById("counter");
 
 window.onscroll = function(){
 
   var distance = window.pageYOffset;
 
 
-  counter.innerHTML = distance + "px";
+
 
   if (distance > 300) {
-    counter.style.backgroundColor = "green";
-    counter.style.top = 200 + distance * 0.9 + "px";
+    counter.style.top = 200 + distance;
 
 
 
   }else{
-    counter.style.backgroundColor = "blue";
+    counter.style.backgroundColor = "black";
     counter.style.top = 200 + distance + "px";
     }
 
@@ -27,3 +25,6 @@ window.onscroll = function(){
     counter.style.left = 600 - distance + "px";
 
     }
+
+
+};
