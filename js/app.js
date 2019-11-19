6124743539e85043b7d022e58c56b211b0235e13
Gabriel Fletcher <gabriel.fletcher@csueastbay.edu>
2019-11-19 11:33:17 -0800

    more changes

--- js/app.js
+++ js/app.js
@@ -1,15 +1,20 @@
-var canvas = document.getElementById("canvas");
 
-window.onscroll = function(){
+//coding from https://codepen.io/rachsmith/pen/PqPNqW//
+var container = document.getElementById('container');
+var windowHeight = window.innerHeight;
+var windowWidth = window.innerWidth;
+var scrollArea = 1000 - windowHeight;
+var square1 = document.getElementsByClassName('square')[0];
+var square2 = document.getElementsByClassName('square')[1];
 
-  var distance = window.pageYOffset;
+// update position of square 1 and square 2 when scroll event fires.
+window.addEventListener('scroll', function() {
+  var scrollTop = window.pageYOffset || window.scrollTop;
+  var scrollPercent = scrollTop/scrollArea || 1;
 
 
-  if (distance < 800) {
+  square1.style.left = scrollPercent*window.innerWidth + 'px';
+  square2.style.top = 800 - scrollPercent*window.innerWidth*0.6 + 'px';
+  square2.style.top = 800 + 'px';
 
-  }
-  else (distance > 800 && distance < 4000){
-    carriage.style.left = distance * 1 + "px";
-  }
-
-};
+});
