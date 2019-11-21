bf0ba673598815cc758a89c3a0fe4026cfede26b
Gabriel Fletcher <gabriel.fletcher@csueastbay.edu>
2019-11-21 13:42:32 -0800

    change

--- js/app.js
+++ js/app.js
@@ -7,14 +7,37 @@ var scrollArea = 1000 - windowHeight;
 var square1 = document.getElementsByClassName('square')[0];
 var square2 = document.getElementsByClassName('square')[1];
 
-// update position of square 1 and square 2 when scroll event fires.
 window.addEventListener('scroll', function() {
   var scrollTop = window.pageYOffset || window.scrollTop;
   var scrollPercent = scrollTop/scrollArea || 1;
 
 
   square1.style.left = scrollPercent*window.innerWidth + 'px';
-  square2.style.top = 800 - scrollPercent*window.innerWidth*0.6 + 'px';
-  square2.style.top = 800 + 'px';
+
+
 
 });
+//coding from https://jsbin.com/waducab/3/edit?html,css,js,output//
+var canvas = document.getElementById("canvas");
+var counter = document.getElementById("counter");
+
+window.onscroll = function(){
+
+
+  var distance = window.pageYOffset;
+
+
+  if (distance < 900) {
+  counter.style.position="fixed"
+  outfit.style.position="fixed"
+}
+else if (distance > 900){
+  counter.style.left = 1100 - distance + "px";
+  outfit.style.left =1100 - distance + "px";
+}
+else {
+  counter.style.position = "absolute";
+}
+
+
+};
