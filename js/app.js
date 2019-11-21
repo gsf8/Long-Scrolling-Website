92ec260d86f65020045a9495b21401a5459fe750
Gabriel <gabriel.fletcher@csueastbay.edu>
2019-11-21 14:35:41 -0800

    fixed javascript
    hidden overflow not working

--- js/app.js
+++ js/app.js
@@ -7,37 +7,63 @@ var scrollArea = 1000 - windowHeight;
 var square1 = document.getElementsByClassName('square')[0];
 var square2 = document.getElementsByClassName('square')[1];
 
+//coding reference from https://jsbin.com/waducab/3/edit?html,css,js,output//
+var canvas = document.getElementById("canvas");
+var counter = document.getElementById("counter");
+
+
+
 window.addEventListener('scroll', function() {
   var scrollTop = window.pageYOffset || window.scrollTop;
   var scrollPercent = scrollTop/scrollArea || 1;
 
+  // var distance = window.pageYOffset;
 
-  square1.style.left = scrollPercent*window.innerWidth + 'px';
+
+  console.log(scrollTop);
+
+  // square1.style.left = scrollPercent*window.innerWidth + 'px';
+
+  counter.style.top = scrollTop+ 50+ 'px';
+  outfit.style.top = scrollTop+ 50 + 'px';
+
+//   if (scrollTop < 900) {
+//   counter.style.position="fixed"
+//   outfit.style.position="fixed"
+// }
+// else if (scrollTop > 900){
+//   counter.style.left = 1100 - scrollTop + "px";
+//   outfit.style.left =1100 - scrollTop + "px";
+// }
+// else {
+//   counter.style.position = "absolute";
+// }
+
+
+switch (true) {
+
+  case (scrollTop < 100):
+  counter.style.position = "absolute";
+  outfit.style.position="absolute"
+  counter.style.left = "450px";
+  outfit.style.left ="500px";
+  break;
+
+  case (scrollTop < 900):
+  // counter.style.position="fixed"
+  // outfit.style.position="fixed"
+  break;
+
+  case (scrollTop < 1100):
+  counter.style.left = 1100 - scrollTop + "px";
+  outfit.style.left =1100 - scrollTop + "px";
+  break;
+
+  default:
+  counter.style.position = "absolute";
+  outfit.style.position="absolute"
+}
 
 
 
 });
-//coding from https://jsbin.com/waducab/3/edit?html,css,js,output//
-var canvas = document.getElementById("canvas");
-var counter = document.getElementById("counter");
-
-window.onscroll = function(){
-
-
-  var distance = window.pageYOffset;
-
-
-  if (distance < 900) {
-  counter.style.position="fixed"
-  outfit.style.position="fixed"
-}
-else if (distance > 900){
-  counter.style.left = 1100 - distance + "px";
-  outfit.style.left =1100 - distance + "px";
-}
-else {
-  counter.style.position = "absolute";
-}
-
-
-};
