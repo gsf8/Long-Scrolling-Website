33dbf9e2d8b15ff89303b8fb80a701460d560d41
Gabriel Fletcher <gabriel.fletcher@csueastbay.edu>
2019-12-10 09:27:31 -0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -20,29 +20,29 @@ window.onscroll = function(){
 
 
   if(y > 1000){
-    icon.src="../images/zero.png"
+    icon.src="images/zero.png"
 
    }else{
-   icon.src="../images/baby.png";
+   icon.src="images/baby.png";
 
    }
    if(y > 5000){
-     icon.src="../images/four.png"
+     icon.src="images/four.png"
 
 
     }
     if(y > 8000){
-      icon.src="../images/ten.png"
+      icon.src="images/ten.png"
 
 
      }
      if(y > 11000){
-       icon.src="../images/sixteen.png"
+       icon.src="images/sixteen.png"
        icon.style.top = 100 + y + "px";
 
       }
     if(y > 18000){
-      icon.src="../images/twentythree.png"
+      icon.src="images/twentythree.png"
 
 
      }
